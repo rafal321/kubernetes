01f5895d165cc81300e5aa9163cb065dcd5bcddd
therafk <therafk@hotmail.com>
2019-01-10 13:35:15 +0000

    var let const

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 "use strict"
 
-
+/*
 var a = 5;
 let b = 'Text';
 console.log(b);
+
+//-------var vs let----------------
+function testLet() {
+    let a = 30;
+    if (true) {
+        let a = 50;
+        console.log(a)
+    }
+    console.log(a)
+}
+testLet();
+//---------------------------
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log(i);
+
+for (let x = 0; x < 10; x++) {
+    console.log(x);
+}
+console.log(x);     //Uncaught ReferenceError: x is not defined - out of scope
+
+//---------------------------
+
+const colors = [];
+
+colors.push('red');
+colors.push('blue');
+console.log(colors);
+colors = 'green';       //ypeError: Assignment to constant variable
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
