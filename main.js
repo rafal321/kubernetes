6a6093407bcba55ecd1797c0ec82bbbf3104494b
therafk <therafk@hotmail.com>
2019-01-10 14:00:59 +0000

    classes+inheritance

--- main.js
+++ main.js
@@ -35,17 +35,46 @@ colors.push('blue');
 console.log(colors);
 colors = 'green';       //ypeError: Assignment to constant variable
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+//--------------------------------
+//------- classes ----------------
+class User {
+    //ctr are methods that will run when your class is instanciated
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    //method - a function belonging to a class
+    registered() {
+        console.log(this.username + ' is registered');
+    }
+    //static method
+    static countUsers() {
+        console.log('there are 50 users');
+    }
+}
 
+let bob = new User('bob', 'bob@bob.com', '123abc');
 
+console.log(bob);
+bob.registered();
+console.log(bob.email);
+//call static method
+User.countUsers();
+// extend classes/inheritance 
 
+class Member extends User {
+    constructor(username, email, password, memberPackage){
+    super(username, email, password);
+    this.package = memberPackage;
+    }
 
+    getPackage(){
+        console.log(this.username + ' has ' + this.package)
+    }
+}
 
+let mike = new Member('Mike', 'm@m.com', '890', 'gold');
 
-
-
-
-
-
-
-
-
+mike.getPackage();
+mike.registered();
